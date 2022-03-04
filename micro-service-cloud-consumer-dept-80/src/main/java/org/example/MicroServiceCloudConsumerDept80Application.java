d3b8e87f11f5b7bd5593c4b0867fd785bb3d69f2
guocz <guocz@gdyhsp.com>
2022-03-04 15:40:50 +0800

    服务注册中心eureka集群，定制负载均衡策略

--- micro-service-cloud-consumer-dept-80/src/main/java/org/example/MicroServiceCloudConsumerDept80Application.java
+++ micro-service-cloud-consumer-dept-80/src/main/java/org/example/MicroServiceCloudConsumerDept80Application.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.myrule.MySelfRibbonRuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
+// 指定负载均衡策略为定制负载均衡策略
+@RibbonClient(name = "MICROSERVICECLOUDPROVIDERDEPT", configuration = MySelfRibbonRuleConfig.class)
 public class MicroServiceCloudConsumerDept80Application {
 
     public static void main(String[] args) {
--- micro-service-cloud-consumer-dept-80/src/main/java/org/example/config/ConfigBean.java
+++ micro-service-cloud-consumer-dept-80/src/main/java/org/example/config/ConfigBean.java
@@ -1,5 +1,7 @@
 package org.example.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +18,21 @@ public class ConfigBean {
     @LoadBalanced // 在客户端使用 RestTemplate 请求服务端时，开启负载均衡（Ribbon）
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * 负载均衡策略
+     * 默认：轮询
+     * RoundRobinRule-轮询、RandomRule-随机、RetryRule-重试、WeightedResponseTimeRule-权重
+     * BestAvailableRule-先过滤点故障或失效的服务实例，然后再选择并发量最小的服务实例。
+     * AvailabilityFilteringRule-先过滤掉故障或失效的服务实例，然后再选择并发量较小的服务实例。
+     * ZoneAvoidanceRule-默认的负载均衡策略，综合判断服务所在区域（zone）的性能和服务（server）的可用性，来选择服务实例。在没有区域的环境下，该策略与轮询（RandomRule）策略类似。
+     * @return
+     */
+    @Bean
+    public IRule myRule() {
+        // 随机策略
+        return new RandomRule();
     }
 
 }
